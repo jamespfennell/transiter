02bb69a76b4ce7fc548cc62096bf507824db9df5
James Fennell <jamespfennell@gmail.com>
2018-09-18 19:15:34 -0400

    100% tests for graphs

--- tests/utils/graphutils/test_graphdatastructs.py
+++ tests/utils/graphutils/test_graphdatastructs.py
@@ -5,18 +5,23 @@ from transiter.utils.graphutils import graphdatastructs
 class TestGraphDataStructs(unittest.TestCase):
     def setUp(self):
         edges = [('a', 'b'), ('c', 'd'), ('b', 'c')]
-        self.graph_one = graphdatastructs.construct_graph_from_edge_tuples(edges)
+        self.graph_1 = graphdatastructs.construct_graph_from_edge_tuples(edges)
         edges = [('a', 'b'), ('c', 'd'), ('c', 'b')]
-        self.graph_two = graphdatastructs.construct_graph_from_edge_tuples(edges)
+        self.graph_2 = graphdatastructs.construct_graph_from_edge_tuples(edges)
+        edges = [('a', 'b'), ('b', 'c'), ('b', 'd')]
+        self.graph_3 = graphdatastructs.construct_graph_from_edge_tuples(edges)
 
     def test_is_path_1(self):
-        self.assertTrue(self.graph_one.is_path())
+        self.assertTrue(self.graph_1.is_path())
 
     def test_is_path_2(self):
-        self.assertFalse(self.graph_two.is_path())
+        self.assertFalse(self.graph_2.is_path())
+
+    def test_is_path_3(self):
+        self.assertFalse(self.graph_3.is_path())
 
     def test_cast_to_path_1(self):
-        path = self.graph_one.cast_to_path()
+        path = self.graph_1.cast_to_path()
         expected = ['a', 'b', 'c', 'd']
         i = 0
         for vertex in path.vertices():
@@ -25,4 +30,8 @@ class TestGraphDataStructs(unittest.TestCase):
 
     def test_cast_to_path_2(self):
         self.assertRaises(graphdatastructs.NotCastableAsAPathError,
-            self.graph_two.cast_to_path)
+            self.graph_2.cast_to_path)
+
+    def test_cast_to_path_3(self):
+        self.assertRaises(graphdatastructs.NotCastableAsAPathError,
+            self.graph_3.cast_to_path)
