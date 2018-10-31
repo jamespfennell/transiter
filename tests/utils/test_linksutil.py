7e2b34927a88f9f9ff7497d7c97741843b7c0f49
James Fennell <jamespfennell@gmail.com>
2018-10-30 22:14:06 -0400

    Tests for links util

---
+++ tests/utils/test_linksutil.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest import mock
+from transiter.utils import linksutil
+
+
+class TestLinks(unittest.TestCase):
+
+    URL = 'Url'
+
+    @mock.patch('transiter.utils.linksutil.flask')
+    def test_entity_links(self, flask):
+        flask.url_for.return_value = self.URL
+        model = mock.MagicMock()
+        link_classes = [
+            linksutil.FeedEntityLink,
+            linksutil.FeedsInSystemIndexLink,
+            linksutil.StopEntityLink,
+            linksutil.StopsInSystemIndexLink,
+            linksutil.SystemEntityLink,
+            linksutil.RouteEntityLink,
+            linksutil.RoutesInSystemIndexLink,
+            linksutil.TripEntityLink
+        ]
+        for link_class in link_classes:
+            link = link_class(model)
+
+            actual = link.url()
+
+            self.assertEqual(actual, self.URL)
+
+            flask.url_for.assert_called_with(
+                link.endpoint,
+                _external=True,
+                **link.kwargs)
+
+
+    def test_broken_link(self):
+        link = linksutil.Link()
+
+        self.assertRaises(NotImplementedError, link.url)
+
+
--- transiter/utils/linksutil.py
+++ transiter/utils/linksutil.py
@@ -13,10 +13,6 @@ class Link:
         return flask.url_for(self.endpoint, _external=True, **self.kwargs)
 
 
-class EntityLink:
-    pass
-
-
 class FeedEntityLink(Link):
 
     def __init__(self, feed):
@@ -93,75 +89,3 @@ class TripEntityLink(Link):
             'trip_id': trip.trip_id
         }
 
-"""
-class IndexLink(Link):
-    def __init__(self, model):
-        self.model = model
-
-    def url(self):
-        return model_to_index_url(self)
-
-
-class EntityLink(Link):
-
-    def __init__(self, entity):
-        self.entity = entity
-
-    def url(self):
-        return entity_to_entity_url(self.entity)
-
-
-def url_func(endpoint, **kwargs):
-    return flask.url_for(endpoint, _external=True, **kwargs)
-
-
-model_to_index_parser = {
-
-}
-
-
-def model_to_index_url(model):
-    return 'model'
-    pass
-
-
-def trip_entity_parser(func, trip):
-    return func(
-        'trip_endpoints.get_in_route_by_id',
-        system_id=trip.route.system_id,
-        route_id=trip.route.route_id,
-        trip_id=trip.trip_id)
-
-
-def route_entity_parser(func, route):
-    return func(
-        'route_endpoints.get_in_system_by_id',
-        system_id=route.system_id,
-        route_id=route.route_id)
-
-
-def stop_entity_parser(func, stop):
-    return func(
-        'stop_endpoints.get_in_system_by_id',
-        system_id=stop.system_id,
-        stop_id=stop.stop_id)
-
-    # _external=True)
-
-
-entity_type_to_entity_parser = {
-    models.Stop: stop_entity_parser,
-    models.Route: route_entity_parser,
-    models.Trip: trip_entity_parser
-}
-
-
-def entity_to_entity_url(entity):
-
-    parser = entity_type_to_entity_parser.get(type(entity), None)
-    if parser is None:
-        # raise NotImplementedError
-        return 'NOT IMPLEMENTED'
-    return parser(url_func, entity)
-
-"""
