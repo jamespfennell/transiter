156543a739bb7b4cf39f47b2ddf970dfa1b7491e
james <jamespfennell@gmail.com>
2019-09-24 22:06:05 -0400

    Push to Docker Hub on master builds

---
+++ docker/docker_hub_push.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+if ([[ "$TRAVIS_BRANCH" == "master" ]] || [[ ! -z "$TRAVIS_TAG" ]]) && [[ "$TRAVIS_PULL_REQUEST" == "false" ]];
+then
+    docker push "jamespfennell/transiter:latest-webserver"
+    docker push "jamespfennell/transiter:latest-taskserver"
+    docker push "jamespfennell/transiter:latest-postgres"
+fi;
