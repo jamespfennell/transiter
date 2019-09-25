bbfb0181f0eab2509930e8c393b19e89d230ad9a
james <jamespfennell@gmail.com>
2019-09-24 22:18:29 -0400

    Double check:

--- docker/docker_hub_push.sh
+++ docker/docker_hub_push.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Pushing Docker images to Docker Hub"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 if ([[ "$TRAVIS_BRANCH" == "master" ]] || [[ ! -z "$TRAVIS_TAG" ]]) && [[ "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
