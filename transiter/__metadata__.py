e26b67251147a874ec2257840a77d664c0fe598e
james <jamespfennell@gmail.com>
2020-05-03 21:32:14 -0400

    Bump version

--- transiter/__metadata__.py
+++ transiter/__metadata__.py
@@ -1,5 +1,5 @@
 # When updating the version, also update it in the docs.
-__version__ = "0.4.4"
+__version__ = "0.5.0"
 
 # The following are set by Travis CI at build time.
 __build_number__ = None
