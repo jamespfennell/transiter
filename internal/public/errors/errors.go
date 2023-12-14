4f3365b1f061c2c82fba672d8ac35d351cdab3c0
James Fennell <jamespfennell@gmail.com>
2023-12-14 17:53:04 -0500

    Make API error logs slightly better (#133)

--- internal/public/errors/errors.go
+++ internal/public/errors/errors.go
@@ -57,6 +57,7 @@ func GetStatusCode(err error) codes.Code {
 // ServeMuxOption returns a `runtime.ServeMuxOption` that makes errors user-friendly at the API boundary.
 func ServeMuxOption(logger *slog.Logger) runtime.ServeMuxOption {
 	return runtime.WithErrorHandler(func(ctx context.Context, sm *runtime.ServeMux, m runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+		logger = logger.With(slog.String("http_method", r.Method), slog.String("http_url_path", r.URL.Path))
 		switch err.(type) {
 		case publicErr:
 			// nothing to do
@@ -65,14 +66,14 @@ func ServeMuxOption(logger *slog.Logger) runtime.ServeMuxOption {
 		}:
 			// nothing to do
 		default:
-			logger.ErrorCtx(ctx, fmt.Sprintf("unexpected internal error: %s", err))
+			logger.WarnCtx(ctx, fmt.Sprintf("API error of unknown type: %s", err))
 			s := status.New(codes.Internal, "internal error")
 			s, _ = s.WithDetails(status.Convert(err).Proto())
 			err = publicErr{
 				Status: s,
 			}
 		}
-		logger.DebugCtx(ctx, fmt.Sprintf("returning API error: %s", err))
+		logger.DebugCtx(ctx, fmt.Sprintf("API error: %s", err))
 		runtime.DefaultHTTPErrorHandler(ctx, sm, m, w, r, err)
 	})
 }
